39edbb354c786d0c63a72a3e3c4de376b80d7faa
noobmaster432 <mrgyan432@gmail.com>
2022-08-21 11:59:06 +0530

    Ab to chal jaa

--- app.js
+++ app.js
@@ -128,10 +128,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-app.listen(port,()=>{
-  console.log("app started");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
